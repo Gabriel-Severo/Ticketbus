0967dfcd090d5538ca9bad7d060c9e0f85a2c031
gabriel-severo <gabriel.ivi@hotmail.com>
2020-03-28 11:02:33 -0300

    Adding poltrona selection

--- bus.js
+++ bus.js
@@ -1,3 +1,9 @@
 document.getElementsByClassName('nome')[0].innerHTML = sessionStorage.getItem('nome')
 document.getElementsByClassName('origem')[0].innerHTML = sessionStorage.getItem('origem')
 document.getElementsByClassName('destino')[0].innerHTML = sessionStorage.getItem('destino')
+
+document.querySelectorAll('.poltrona').forEach((e) => {
+    e.addEventListener('click', () => {
+        document.getElementsByClassName('poltrona-texto')[0].innerHTML = e.innerHTML
+    })
+})
