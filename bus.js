56542b8b29e5105a6e112440bbea2632899da7cc
gabriel-severo <gabriel.ivi@hotmail.com>
2020-03-29 12:18:55 -0300

    Adding function to recibo area

--- bus.js
+++ bus.js
@@ -2,6 +2,12 @@ document.getElementsByClassName('nome')[0].innerHTML = sessionStorage.getItem('n
 document.getElementsByClassName('origem')[0].innerHTML = sessionStorage.getItem('origem')
 document.getElementsByClassName('destino')[0].innerHTML = sessionStorage.getItem('destino')
 
+const date = new Date()
+data = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear()
+hora = date.getHours() + ':' + date.getMinutes()
+document.getElementsByClassName('data')[0].innerHTML = data
+document.getElementsByClassName('hora')[0].innerHTML = hora
+
 document.querySelectorAll('.poltrona').forEach((e) => {
     e.addEventListener('click', () => {
         const selecionado = document.querySelector('.selecionado')
