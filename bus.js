5bdd3546a2f7231f4de4c776d1abcbd5d9cc183d
gabriel-severo <gabriel.ivi@hotmail.com>
2020-03-29 11:21:05 -0300

    Improving poltrona selection

--- bus.js
+++ bus.js
@@ -4,6 +4,13 @@ document.getElementsByClassName('destino')[0].innerHTML = sessionStorage.getItem
 
 document.querySelectorAll('.poltrona').forEach((e) => {
     e.addEventListener('click', () => {
+        const selecionado = document.querySelector('.selecionado')
+        if (selecionado != null){
+            selecionado.classList.toggle('selecionado')
+            selecionado.classList.toggle('poltrona')
+        }
         document.getElementsByClassName('poltrona-texto')[0].innerHTML = e.innerHTML
+        e.classList.toggle('selecionado')
+        e.classList.toggle('poltrona')
     })
 })
